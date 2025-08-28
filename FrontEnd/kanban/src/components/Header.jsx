9f937c3a96e1c705eb1a33f9d9a146935a4a6244
Joao Adegas <pedrojoao6378@gmail.com>
2025-08-28 08:51:23 -0300

    header

---
+++ FrontEnd/kanban/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import "../styles/Header.scss"
+
+export default function header(){
+    return(
+        <header>
+            <div className="titlle">
+                <h1 className="tittle-h1">Gerenciamento de Tarefas</h1>
+            </div>
+            <nav>
+                <ul className="ul-list">
+                    <li><a href="#" className="link-header" >Cadastro de Usuário</a> </li>
+                    <li><a href="#" className="link-header" >Cadastro de Taredas</a> </li>
+                    <li><a href="#" className="link-header" >Gerecnaiemnto de Tarefas</a> </li>
+                </ul>
+            </nav>
+        </header>
+    )
+
+}
